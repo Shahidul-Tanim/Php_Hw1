05f55928dbf2f0e51d0a56d85e6923657e62c974
Shahidul-Tanim <115738942+Shahidul-Tanim@users.noreply.github.com>
2023-12-10 23:37:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "Tanim";
+$age = "23";
+
+echo " I am $name. I am $age years old. <br>";
+echo " I am $name. I am $age years old. <br>";
+echo " I am $name. I am $age years old. <br>";
+echo " I am $name. I am $age years old. <br>";
+echo " I am $name. I am $age years old. <br>";
+
+define('PI', 3.42);
+
+echo PI;
+
